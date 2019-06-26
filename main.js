fa0f5c2556a0ca8803ed190800e5e8a6e885ab3e
jhcamayo7 <jhcamayo7@misena.edu.co>
2019-06-26 11:49:53 -0500

    metodo string

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
 //alert("ESTAMOS EMPEZANDO A USAR GIT")
 //console.log("AHORA VAMOS A SUBIR NUESTROS ARCHIVOS A GIT HUB")
-var persona= {
-	nombre: "Axel",
-	apellido:"Franco",
-	cedula:"1234567895",
-	 edad: 18
-}
-if(persona.edad >= 18)
-{
-alert(persona.nombre+" "+"es mayor edad y tiene "+ persona.edad )
-}
+
+//  var persona = [
+// 	{nombre: "Jaime",
+// 	apellido:"Campo",
+// 	cedula:"1234567895",
+// 	 edad: 17,
+// 	 PeliclaFavorita:"Los Secretos de Transilvania",
+// 	 Genero:"Terror"
+// },
+// {
+// 	nombre: "Laura",
+// 	apellido:"Franco",
+// 	cedula:"5678912348",
+// 	 edad: 8,
+// 	 PeliclaFavorita:"Rapido y Furioso",
+// 	 Genero:"Accion"
+
+// },
+//  {
+// 	nombre: "Karen",
+// 	apellido:"Vargas",
+// 	cedula:"7891234561",
+// 	 edad: 19,
+// 	 PeliclaFavorita:"Piratas del Caribe",
+// 	 Genero:"Accion"
+// 	 }
+
+// ]
+// for (var i = 0; i <= persona.length; i++) {
+// 	alert(persona[i].nombre+"\n"+persona[i].apellido+"\n"+persona[i].cedula+"\n"+persona[i].edad+"\n"+persona[i].PeliclaFavorita+"\n"+persona[i].Genero)
+// 	if (persona[i].edad>=18) {
+// 		alert("te recomendamos estas tres peliculas")
+// 		alert("piratas del caribe, duro de matar, anabelle")
+
+// 		}else{
+// 			alert("te recomendamos estas tres peliculas")
+// 			alert("toy story, la era de hielo, moana")
+// 			}
+// 	}
+var cadena1="carro bmw 2019"
+console.log(cadena1.substring(4,7))
+// console.log(cadena1.split(" "))
+// var cadena2="nuevo"
+
+// console.log(cadena1.length)
+
+// console.log(cadena1.charAt(0))
+// console.log(cadena1.charAt(1))
+// console.log(cadena1.charAt(2))
+// console.log(cadena1.charAt(3))
+// console.log(cadena1.charAt(4))
+
+// console.log(cadena1.toLowerCase())
+// console.log(cadena1.toUpperCase())
+
+// console.log(cadena1.concat(cadena2))
+ // console.log(cadena1.endsWith("o"))
+// console.log(cadena1.includes("rp"))
+// console.log(cadena1.indexOf("bmw"))
+// console.log(cadena1.slice(0,5))
